c855acb86cdbe9c33a4b906d53cde3d3cce63c99
Carlos Eduardo <edugaspar@hotmail.com>
2021-08-22 20:27:44 -0300

    creates basic create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,8 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  /*
+  // Regex rules
+  // Passwords will contain at least 1 upper case letter
+  // Passwords will contain at least 1 lower case letter
+  // Passwords will contain at least 1 number or special character
+  */
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
+  password: string;
+}
