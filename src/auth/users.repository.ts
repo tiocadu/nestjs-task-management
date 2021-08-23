bb3fcaff9d0e4f7d5c23bae36775eeb7536b19e2
Carlos Eduardo <edugaspar@hotmail.com>
2021-08-22 21:03:18 -0300

    add username as unique field

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -12,6 +16,15 @@ export class UsersRepository extends Repository<User> {
       password,
     });
 
-    await this.save(newUser);
+    try {
+      await this.save(newUser);
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException(error.detail);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
