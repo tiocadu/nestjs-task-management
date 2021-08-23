1af4a2a2422f9971754f06c127642891af6696a6
Carlos Eduardo <edugaspar@hotmail.com>
2021-08-22 23:14:18 -0300

    remove unused import

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
