3a3b34dd3d459361c0a12970379c8ce00431512d
Carlos Eduardo <edugaspar@hotmail.com>
2021-08-28 19:56:31 -0300

    adds basic logging capabilities

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -5,9 +5,12 @@ import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { User } from 'src/auth/user.entity';
 import { GetUser } from 'src/auth/get-user.decorator';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TasksRepository', { timestamp: true });
+
   async getTasks(
     filterDto: GetTasksFilterDto,
     @GetUser() user: User,
@@ -29,8 +32,18 @@ export class TasksRepository extends Repository<Task> {
       );
     }
 
-    const queryResult = await query.getMany();
-    return queryResult;
+    try {
+      const queryResult = await query.getMany();
+      return queryResult;
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user "${
+          user.username
+        }", Filters: ${JSON.stringify(filterDto)}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
